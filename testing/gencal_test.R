4e9f0fb08a1807e8cd1b04e1c04f8b41acfca7a0
Maciej Ostapiuk <maciej.ostapiuk@interia.pl>
2024-08-13 17:10:03 +0200

    Performed testing od gencal (dim(xs) == dim(zs) and dim(xs) <= dim(zs) as well)

--- testing/gencal_test.R
+++ testing/gencal_test.R
@@ -8,7 +8,7 @@ pr <- plogis(1 + 0.5*x1 - 0.5*y)
 pop_data <- data.frame(x1, x2, y, pr)
 
 totals <- c(N = n, colSums(pop_data[, c("x1", "x2")]))
-
+### WHEN DIM(response model variables ) <= DIM(calibration model variables)
 n_reps <- 10000
 results <- matrix(0, n_reps, 2)
 
@@ -16,21 +16,52 @@ for (r in 1:n_reps) {
   flag <- rbinom(n, 1, pop_data$pr)
   sample <- pop_data[flag == 1, ]
   xs = model.matrix(~x1 + x2, sample)
-  zs = model.matrix(~x1 + y, sample)
+  zs = model.matrix(~x1+x2+y, sample)
   d = rep(1, NROW(sample))
 
   # Generalized calibration
-  g_generl <- mnar(totals = totals, nonresponse = xs, calib_var = zs, target_var = y, initial_weights = d, method = "gencalib")
+  g_generl <- gencal(totals = totals, nonresponse = xs, calib_var = zs, initial_weights = d)
 
   # Naive
   results[r, 1] <- mean(sample$y)
   results[r, 2] <- weighted.mean(sample$y, g_generl)
 }
 
-boxplot(results - mean(pop_data$y))
+boxplot(results - mean(pop_data$y), ylim = c(-0.7, 0.3))
 abline(h = 0, col = "red")
 
 y_true <- pop_data$y
 apply(results, 2, FUN = function(x) c(bias = mean(x) - mean(y_true),
                                       sd = sd(x),
                                       rmse = sqrt( (mean(x) - mean(y_true))^2 + var(x))))
+
+
+
+
+### WHEN DIM(response model variables ) == DIM(calibration model variables)
+
+for (r in 1:n_reps) {
+  flag <- rbinom(n, 1, pop_data$pr)
+  sample <- pop_data[flag == 1, ]
+  xs = model.matrix(~x1 + x2, sample)
+  zs = model.matrix(~x1+y, sample)
+  d = rep(1, NROW(sample))
+
+  # Generalized calibration
+  g_generl <- gencal(totals = totals, nonresponse = xs, calib_var = zs, initial_weights = d)
+
+  # Naive
+  results[r, 1] <- mean(sample$y)
+  results[r, 2] <- weighted.mean(sample$y, g_generl)
+}
+
+boxplot(results - mean(pop_data$y), ylim = c(-0.7, 0.3))
+abline(h = 0, col = "red")
+
+y_true <- pop_data$y
+apply(results, 2, FUN = function(x) c(bias = mean(x) - mean(y_true),
+                                      sd = sd(x),
+                                      rmse = sqrt( (mean(x) - mean(y_true))^2 + var(x))))
+
+
+
