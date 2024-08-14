86dec8578f59f9ac9b2a4068f1c6f8ace652f4ed
Maciej Ostapiuk <maciej.ostapiuk@interia.pl>
2024-08-14 20:10:19 +0200

    Performed a MC simulation in order to determine whether gencal() function works well or not

--- testing/gencal_test.R
+++ testing/gencal_test.R
@@ -1,30 +1,27 @@
 set.seed(123)
-n <- 10000
+n <- 1000
 x1 <- rlnorm(n, 0, 1)
 x2 <- rexp(n, 1)
 x3 <-  rpois(n,2)
 y <- 1 + x1 + x2 + rnorm(n)
 pr <- plogis(1 + 0.5*x1 - 0.5*y)
 pop_data <- data.frame(x1, x2, y, pr)
-
-totals <- c(N = n, colSums(pop_data[, c("x1", "x2")]))
-### WHEN DIM(response model variables ) <= DIM(calibration model variables)
-n_reps <- 10000
-results <- matrix(0, n_reps, 2)
+totals <- c(N = n, colSums(pop_data[, c("x1", "y")]))
 
 for (r in 1:n_reps) {
   flag <- rbinom(n, 1, pop_data$pr)
   sample <- pop_data[flag == 1, ]
-  xs = model.matrix(~x1 + x2, sample)
-  zs = model.matrix(~x1+x2+y, sample)
-  d = rep(1, NROW(sample))
-
+  sample$d <- n/nrow(sample)
   # Generalized calibration
-  g_generl <- gencal(totals = totals, nonresponse = xs, calib_var = zs, initial_weights = d)
+  g <- mnar(response = ~ x1 + y,
+            calibration =  ~ x1 + x2,
+            data = sample, dweights = sample$d,
+            pop_totals = totals,
+            method = "gencalib")
 
   # Naive
   results[r, 1] <- mean(sample$y)
-  results[r, 2] <- weighted.mean(sample$y, g_generl)
+  results[r, 2] <- weighted.mean(sample$y, g)
 }
 
 boxplot(results - mean(pop_data$y), ylim = c(-0.7, 0.3))
@@ -36,23 +33,20 @@ apply(results, 2, FUN = function(x) c(bias = mean(x) - mean(y_true),
                                       rmse = sqrt( (mean(x) - mean(y_true))^2 + var(x))))
 
 
-
-
-### WHEN DIM(response model variables ) == DIM(calibration model variables)
-
 for (r in 1:n_reps) {
   flag <- rbinom(n, 1, pop_data$pr)
   sample <- pop_data[flag == 1, ]
-  xs = model.matrix(~x1 + x2, sample)
-  zs = model.matrix(~x1+y, sample)
-  d = rep(1, NROW(sample))
-
+  sample$d <- n/nrow(sample)
   # Generalized calibration
-  g_generl <- gencal(totals = totals, nonresponse = xs, calib_var = zs, initial_weights = d)
+  g1 <- mnar(response = ~ x1 + y,
+             calibration =  ~ x1 + x2 +y,
+             data = sample, dweights = sample$d,
+             pop_totals = totals,
+             method = "gencalib")
 
   # Naive
   results[r, 1] <- mean(sample$y)
-  results[r, 2] <- weighted.mean(sample$y, g_generl)
+  results[r, 2] <- weighted.mean(sample$y, g1)
 }
 
 boxplot(results - mean(pop_data$y), ylim = c(-0.7, 0.3))
