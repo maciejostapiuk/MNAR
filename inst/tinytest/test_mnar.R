3c8bf06f5ffa3e72f66ec3e9e24bfa53c0dfb779
Maciej Ostapiuk <maciej.ostapiuk@interia.pl>
2024-08-14 20:11:08 +0200

    added testing for gencalib with both settings; results were ok

--- inst/tinytest/test_mnar.R
+++ inst/tinytest/test_mnar.R
@@ -1,4 +1,30 @@
+set.seed(123)
+n <- 10000
+x1 <- rlnorm(n, 0, 1)
+x2 <- rexp(n, 1)
+x3 <-  rpois(n,2)
+y <- 1 + x1 + x2 + rnorm(n)
+pr <- plogis(1 + 0.5*x1 - 0.5*y)
+pop_data <- data.frame(x1, x2, y, pr)
+totals <- c(N = n, colSums(pop_data[, c("x1", "x2")]))
+flag <- rbinom(n, 1, pop_data$pr)
+sample <- pop_data[flag == 1, ]
+sample$d <- n/nrow(sample)
 
-# place for tests
-expect_equal(1 + 1, 2)
+# check if it is working --------------------------------------------------
 
+expect_silent(
+  g <- mnar(response = ~ x1 + x2,
+            calibration =  ~ x1 + y,
+            data = sample, dweights = sample$d,
+            pop_totals = totals,
+            method = "gencalib")
+)
+
+expect_silent(
+  g1 <- mnar(response = ~ x1+x2,
+             calibration =  ~ x1 + x2+y,
+             data = sample, dweights = sample$d,
+             pop_totals = totals,
+             method = "gencalib")
+)
